849ac74aa0d14a21bcbb72a22d8f888306be3b34
Heylga <oy.golovina@gmail.com>
2022-04-28 13:18:24 +0000

    navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -11,12 +11,12 @@ export const Navbar = () => {
 			<div className="ml-auto">
 				<div className="dropdown">
   					<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    				Dropdown button
+    				Favourites
   					</button>
   							<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-   						 		<li><a className="dropdown-item" href="#">Action</a></li>
-    							<li><a className="dropdown-item" href="#">Another action</a></li>
-   				 				<li><a className="dropdown-item" href="#">Something else here</a></li>
+   						 		<li><a className="dropdown-item" href="#">Favourite</a></li>
+    							<li><a className="dropdown-item" href="#">Favourite</a></li>
+   				 				<li><a className="dropdown-item" href="#">Favourite</a></li>
   							</ul>
 				</div>
 			</div>
